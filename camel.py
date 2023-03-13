9c1d9fc9e93dd2af5b14b6d7b766d0bdad53ecc9
Bum81ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-03-14 01:03:47 +0400

    Add files via upload

---
+++ camel.py
@@ -0,0 +1,10 @@
+camel = input("CamelCase: ")
+snake = ""
+
+for char in camel:
+    if char.isupper():
+        snake += "_" + char.lower()
+    else:
+        snake += char
+
+print("snake_case:", snake)
